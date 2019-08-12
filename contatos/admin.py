ef64ecad28443c1a5d5bd32eee5efb7f77b38ffa
HeversonSilva <heversonjr@outlook.com>
2019-08-12 20:53:37 -0300

    Adicionado paginação

--- contatos/admin.py
+++ contatos/admin.py
@@ -2,11 +2,12 @@ from django.contrib import admin
 from .models import Categoria, Contato
 
 class ContatoAdmin(admin.ModelAdmin):
-    list_display = ('nome', 'sobrenome', 'descricao')
+    list_display = ('nome', 'sobrenome', 'telefone', 'email', 'descricao', 'listado')
     list_display_links = ('nome', 'sobrenome')
     list_filter = ('nome', 'sobrenome', 'categoria')
     list_per_page = 10
     search_fields = ('nome', 'sobrenome')
+    list_editable = ('telefone', 'email', 'listado')
 
 
 admin.site.register(Contato, ContatoAdmin)
